3c6598b184b34c83a8b2df60651e1fa006047a1b
superasker31-creator <superasker31@gmail.com>
2026-05-25 20:48:29 +0300

    Update Config.java

--- src/main/java/com/esp/Config.java
+++ src/main/java/com/esp/Config.java
@@ -1,1 +1,9 @@
+package com.esp;
+
+public class Config {
+    public static boolean auraEnabled = false;
+    public static double auraRange = 3.6;
+    public static float aimSpeed = 100.0f;
+    public static boolean showHud = true;
+}
 
